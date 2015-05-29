e7426ed8c6e92bb8be3ef9ae9f5f993379fd59e2
MogwaiMomo <momokoprice@gmail.com>
2015-05-28 20:21:56 -0700

    finished styling the discount code field

--- js/payment.js
+++ js/payment.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(
+
 	function() {
 		jQuery("#rcp-stripe-fields > p").append("<div class='credit_card'><img src='http://localhost:8888/watchmecode/wp-content/uploads/2015/05/us_credit_cards.png' alt='us credit card icons'></div>");
-	});
+
+		jQuery( ".discount-link" ).click(function() {
+	  		jQuery( ".rcp_discount_code" ).toggleClass( "hide" );
+		});
+	}
+);
+
+
+
--- rcp/register-single-4.php
+++ rcp/register-single-4.php
@@ -63,21 +63,26 @@ rcp_show_error_messages( 'register' ); ?>
 
 		<?php do_action( 'rcp_after_password_registration_field' ); ?>
 
+	<div id="discount-field"class="one-fourth">
+			<a class="discount-link" href="#">Got a discount code?</a>
+			<?php if( rcp_has_discounts() ) : ?>
+			<fieldset class="rcp_discounts_fieldset">
+				<p id="rcp_discount_code_wrap">
+					<label for="rcp_discount_code">
+						<?php _e( 'Discount Code', 'rcp' ); ?>
+						<span class="rcp_discount_valid" style="display: none;"> - <?php _e( 'Valid', 'rcp' ); ?></span>
+						<span class="rcp_discount_invalid" style="display: none;"> - <?php _e( 'Invalid', 'rcp' ); ?></span>
+					</label>
+					<input type="text" id="rcp_discount_code" name="rcp_discount" class="rcp_discount_code hide" value=""/>
+				</p>
+			</fieldset>
+			<?php endif; ?>
+	</div>
+
+
 	</fieldset>
 	<?php } ?>
 
-	<?php if( rcp_has_discounts() ) : ?>
-	<fieldset class="rcp_discounts_fieldset">
-		<p id="rcp_discount_code_wrap">
-			<label for="rcp_discount_code">
-				<?php _e( 'Discount Code', 'rcp' ); ?>
-				<span class="rcp_discount_valid" style="display: none;"> - <?php _e( 'Valid', 'rcp' ); ?></span>
-				<span class="rcp_discount_invalid" style="display: none;"> - <?php _e( 'Invalid', 'rcp' ); ?></span>
-			</label>
-			<input type="text" id="rcp_discount_code" name="rcp_discount" class="rcp_discount_code" value=""/>
-		</p>
-	</fieldset>
-	<?php endif; ?>
 
 	<?php do_action( 'rcp_after_register_form_fields' ); ?>
 
